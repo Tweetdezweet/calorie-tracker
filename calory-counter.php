577304d6a3f70c1f0be61f5c68ed13ebad311ab9
koengabriels <koen.gabriels@gmail.com>
2019-09-03 20:17:58 +0200

    Initial commit

---
+++ calory-counter.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Calory Tracker
+Plugin URI: https://koengabriels.be/calorytracker
+Description: This plugin helps users track their calories
+Version: 0.0.0
+Author: Koen Gabriels
+Author URI: https://koengabriels.be
+License: GPLv2 or later
+*/
